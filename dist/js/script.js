1b1ef40b2a9c39cb6baa1db8400fd86eb5048380
alyasalsanabilamulyadi@gmail.com <alyasalsanabilamulyadi@gmail.com>
2022-07-05 13:43:59 +0700

    mifify

--- dist/js/script.js
+++ dist/js/script.js
@@ -32,3 +32,34 @@ window.addEventListener('click', function (e) {
         navMenu.classList.add('hidden');
     }
 })
+
+// swiper
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
